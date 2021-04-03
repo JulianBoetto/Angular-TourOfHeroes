8870c7b05740b8df9cfee91046d5f7140a2852cd
julián Boetto <malostratoscba@gmail.com>
2021-04-03 15:44:16 -0300

    2- Mostrar lista

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'; //generamos un archivo (mock-heroes) que simula un JSON
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +9,18 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  // creamos un controlador de evento
+
+  selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+  heroes = HEROES; //recibimos los datos del archivo mock-heroes.ts
   
   constructor() { }
 
