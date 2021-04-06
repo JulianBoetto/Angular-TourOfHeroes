db1941baa3e2ec2f4c234bc911a78e4cba6db020
julián Boetto <malostratoscba@gmail.com>
2021-04-05 22:04:42 -0300

    4- agregar servicios

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'; //generamos un archivo (mock-heroes) que simula un JSON
+import { HeroService } from '../hero.service';// importamos el servicio
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,22 +10,25 @@ import { HEROES } from '../mock-heroes'; //generamos un archivo (mock-heroes) qu
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-  // creamos un controlador de evento
-
   selectedHero?: Hero;
-onSelect(hero: Hero): void {
+  heroes: Hero[] | undefined; //modificamos la propiedad, abajo queda la anterior
+  // heroes = HEROES; //recibimos los datos del archivo mock-heroes.ts
+  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  
+  onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
 
-  heroes = HEROES; //recibimos los datos del archivo mock-heroes.ts
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes(); //agregamos la llamada
   }
 
 }
